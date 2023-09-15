1fff74765f31a3785d6ba78ab192a8580065826b
Oledoid <mertalbahar@windowslive.com>
2023-09-15 13:17:44 +0300

    Multiple image upload/delete done. You can add multiple images for one product. You can delete one or more images from one product. After delete process, the image will be deleted both db and folder.

--- product/admin.py
+++ product/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Category, Product, Image
 from django.utils.safestring import mark_safe
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("productCode", "productName", "productSlug", "isActive", "isHome", "selected_categories",)
@@ -17,12 +20,10 @@ class ProductAdmin(admin.ModelAdmin):
 
         html += "</ul>"
         return mark_safe(html)
+    
+    inlines = [ImageInline]
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("categoryName", "categorySlug",)
     readonly_fields = ("categorySlug",)
-
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ("product",)
